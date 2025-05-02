9e6bf236e0b1639d65c41895854dd2e282b16a5b
jkbniner9 <jkbniner9@gmail.com>
2025-05-02 16:02:47 +0000

    Added basic SessionSettingsForm component

---
+++ frontend/admin/src/components/SessionSettingsForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function SessionSettingsForm() {
+  return (
+    <div>
+      <h2>Session Settings</h2>
+      {/* Form fields will go here */}
+    </div>
+  );
+}
+
+export default SessionSettingsForm;
