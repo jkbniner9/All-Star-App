da212fbd0b2a996758625ca48704735d4bf4b1db
jkbniner9 <jkbniner9@gmail.com>
2025-05-02 20:27:00 +0000

    Added input fields to SessionSettingsForm

--- frontend/admin/src/components/SessionSettingsForm.js
+++ frontend/admin/src/components/SessionSettingsForm.js
@@ -4,9 +4,28 @@ function SessionSettingsForm() {
   return (
     <div>
       <h2>Session Settings</h2>
-      {/* Form fields will go here */}
+      <form>
+        <div>
+          <label htmlFor="rosterSize">Roster Size:</label>
+          <input type="number" id="rosterSize" name="rosterSize" />
+        </div>
+        <div>
+          <label htmlFor="numCoaches">Number of Participating Coaches:</label>
+          <input type="number" id="numCoaches" name="numCoaches" />
+        </div>
+        <div>
+          <label htmlFor="ageCutoff">Age Cutoff Date:</label>
+          <input type="date" id="ageCutoff" name="ageCutoff" />
+        </div>
+        <div>
+          <label htmlFor="accessCode">Access Code (6-digit numeric):</label>
+          <input type="text" id="accessCode" name="accessCode" />
+        </div>
+        <button type="submit">Save Settings</button>
+      </form>
     </div>
   );
 }
 
 export default SessionSettingsForm;
+
